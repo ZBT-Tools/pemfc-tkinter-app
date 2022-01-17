ffd9dd9972b55bf82272f939e862515382164f2e
lufire <lukas.feierabend@gmail.com>
2022-01-17 18:01:49 +0100

    edit setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pemfc-tkinter-app',
+    name='pemfc-tkinter',
     version="0.0.1",
     author="Lukas Feierabend",
     author_email="lukas.feierabend@gmail.com",
